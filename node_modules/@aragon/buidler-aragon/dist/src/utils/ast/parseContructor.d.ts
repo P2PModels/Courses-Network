d137588f6dbc04412abc3cc8c983e5d4168eb508
Daniel Valverde <davalve@amazon.es>
2021-04-16 18:26:00 +0200

    Dani trasteando

--- node_modules/@aragon/buidler-aragon/dist/src/utils/ast/parseContructor.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Returns true if a contract has a constructor, otherwise false.
- *
- * @param sourceCode Source code of the contract.
- */
-export declare function hasConstructor(sourceCode: string): boolean;
-//# sourceMappingURL=parseContructor.d.ts.map
